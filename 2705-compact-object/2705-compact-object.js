a6f2c422ad0194f9f5be07dcd6ea53b43abf291a
muhiyadheenks <muhiyadheensj@gmail.com>
2026-03-10 17:14:44 +0530

    Time: 64 ms (82.22%), Space: 66.3 MB (51.66%) - LeetHub

---
+++ 2705-compact-object/2705-compact-object.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function(obj) {
+    
+  if (Array.isArray(obj)) {
+    return obj
+      .filter(Boolean)
+      .map((item) =>
+        typeof item === "object" && item !== null ? compactObject(item) : item
+      );
+  }
+if (typeof obj === "object" && obj !== null) {
+    const result = {};
+
+    for (let key in obj) {
+      if (Boolean(obj[key])) {
+        result[key] =
+          typeof obj[key] === "object"
+            ? compactObject(obj[key])
+            : obj[key];
+      }}
+      return result
+    }
+return obj
+};
